16fb829715ef0e642d7e7f8864bc351ec8d35b3c
Bill Glover <bill@billglover.co.uk>
2017-05-20 11:29:40 +0100

    Commit intial codebase

--- main.go
+++ main.go
@@ -1,7 +1,146 @@
 package main
 
-import "log"
+import (
+	"crypto/sha256"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type columns []int
+
+func (c *columns) String() string {
+	return fmt.Sprintf("%v$", *c)
+}
+
+func (c *columns) Set(s string) error {
+	a := strings.Split(s, ",")
+	for _, vs := range a {
+		n, err := strconv.Atoi(vs)
+		if err != nil {
+			return fmt.Errorf("unable to parse column number: %v", err)
+		}
+		*c = append(*c, n)
+	}
+
+	return nil
+}
 
 func main() {
-	log.Println("Hello, World!")
+
+	var dcols, mcols columns
+
+	flag.Var(&dcols, "d", "comma separated list of columns to remove (remove takes precidence over mask)")
+	flag.Var(&mcols, "m", "comma separated list of columns to mask")
+	in := flag.String("i", "", "input file (csv formatted)")
+	out := flag.String("o", "", "output file (csv formatted)")
+	header := flag.Bool("header", false, "indicate whether the csv containers a header row")
+
+	flag.Parse()
+
+	inf, err := openInput(*in)
+	if err != nil {
+		log.Fatalf("unable to open input file: %v", err)
+	}
+	defer inf.Close()
+
+	outf, err := openOutput(*out)
+	if err != nil {
+		log.Fatalf("unable to open output file: %v", err)
+	}
+	defer outf.Close()
+
+	err = cleanCSV(inf, outf, dcols, mcols, *header)
+	if err != nil {
+		log.Fatalf("unable to process CSV: %v", err)
+	}
+}
+
+func openInput(path string) (f *os.File, err error) {
+	if path == "" {
+		return os.Stdin, nil
+	}
+
+	f, err = os.Open(path)
+	return
+}
+
+func openOutput(path string) (f *os.File, err error) {
+	if path == "" {
+		return os.Stdout, nil
+	}
+
+	f, err = os.Create(path)
+	return
+}
+
+func cleanCSV(inf io.Reader, outf io.Writer, dcols, mcols columns, header bool) error {
+
+	salt := time.Time.String(time.Now())
+
+	r := csv.NewReader(inf)
+	w := csv.NewWriter(outf)
+
+	for {
+		r, err := r.Read()
+
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		if header {
+			w.Write(r)
+			header = false
+			continue
+		}
+
+		err = maskCols(mcols, &r, salt)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = deleteCols(dcols, &r)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		w.Write(r)
+	}
+	w.Flush()
+
+	return nil
+}
+
+func deleteCols(dcols columns, r *[]string) error {
+	for _, c := range dcols {
+		if c < 0 || c >= len(*r) {
+			return fmt.Errorf("column for deletion is out of bounds: %d is not in the range [%d,%d]", c, 0, len(*r))
+		}
+
+		(*r)[c] = ""
+	}
+	return nil
+}
+
+func maskCols(mcols columns, r *[]string, salt string) error {
+	for _, c := range mcols {
+		if c < 0 || c >= len(*r) {
+			return fmt.Errorf("column for deletion is out of bounds: %d is not in the range [%d,%d]", c, 0, len(*r))
+		}
+
+		val := []string{salt, (*r)[c]}
+		sum := sha256.Sum256([]byte(strings.Join(val, "|")))
+		(*r)[c] = fmt.Sprintf("%x", sum)
+	}
+
+	return nil
 }
